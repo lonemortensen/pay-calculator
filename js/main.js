e6fa383ccbfa5f5358400eb3f089ecbfc49cd275
Lone Mortensen <lmordev@gmail.com>
2025-03-19 15:22:40 -0700

    Added README file and modified comments in js file

--- js/main.js
+++ js/main.js
@@ -1,35 +1,56 @@
 /* ==================================================
 Project description: Pay Calculator
 Author: Lone Mortensen
-Description: 
+Description: The Pay Calculator is a simple web app that 
+takes, verifies, and processes user input and publishes 
+a response to the user interface. The user inputs the 
+number of hours they worked and - using fixed and 
+entirely fictitious hourly and overtime rates - the app 
+calculates and returns the user's pay. 
+Design: The project uses the input html element to
+capture the input entered by the user on the front end.
+The type attributes of the input and button html elements 
+have been modified to allow for JavaScript handling of 
+the input. In the script, a regular expression verifies 
+the input and if statements control the updating of the 
+user interface with two types of error messages or the 
+calculated pay, depending on the input.    
 Built with: HTML, CSS, JavaScript.
 
 ===== *** =====
 
 ================================================== */
 
-
-// Can all of these global var be moved inside the function for encapsulation? calculateButton likely an issue...
-// Consider encapsulation with IIFE instead...
+/**
+ * Global variables. 
+*/
 const form = document.getElementById("workhours-form");
 const errorDisplay = document.querySelector(".error-wrapper");
 const errorMessage = document.querySelector(".error-message");
 const numberInputField = document.getElementById("number-input"); 
 const calculateButton = document.querySelector(".calculate-button");
 const resultMessage = document.querySelector(".result-message");
-const regularHourlyRate = 50; 
-const overtimeHourlyRate = 70;  
 
+/** 
+ * Calculates the pay based on the number of hours entered by the user.
+ * Gets and verifies user input.
+ * Handles user interface display of error messages and calculated pay result.  
+*/
 const calculateTotalPay = () => {
-  const hours = numberInputField.value;
+  const regularHourlyRate = 50; 
+  const overtimeHourlyRate = 70; 
+  const hours = numberInputField.value; // Gets input entered by user. 
   console.log(hours); // works
   
-  /*Verification of user input:*/
-  const inputPattern = /^\d+(\.\d+)?$/; // Regular expression defines a search pattern used to check that input is only numbers; allows decimals, but no negative numbers.
-  // The test() method returns true or false, depending on result.
-  const verifyInput = inputPattern.test(hours); // Checks user input against pattern specified in regular expression.
+  // Verifies user input.
+  // The regular expression search pattern allows numbers only (decimals included, but no negative numbers). 
+  // The test() method checks input against the search pattern and returns true or false. 
+  const inputPattern = /^\d+(\.\d+)?$/; 
+  const verifyInput = inputPattern.test(hours); 
   console.log(verifyInput); // works
 
+  // Displays error message in case of no or invalid input.
+  // Calculates and displays pay result if input is valid.
   if (!hours) {
     errorDisplay.classList.remove("hide-error");
     errorDisplay.classList.add("error-wrapper-styling");
@@ -60,14 +81,20 @@ const calculateTotalPay = () => {
     console.log(totalPay); // works
     resultMessage.scrollIntoView();
     resultMessage.innerText = `${totalPay}`;
-    
   }
 };
 
+/** 
+ * Detects when user clicks the calculate button.
+ * Calls event handler to handle user input and calculate pay.
+ * Clears message and results fields and removes error styling. 
+ * Hides error display field from view.
+ * Clears input field.  
+*/
 calculateButton.addEventListener("click", () => {
   errorMessage.innerText = "";
   resultMessage.innerText = "";
-  errorDisplay.classList.remove("error-wrapper");
+  errorDisplay.classList.remove("error-wrapper-styling");
   errorDisplay.classList.add("hide-error");
   errorMessage.classList.remove("error-icon");
   numberInputField.classList.remove("error-border-input-field");
